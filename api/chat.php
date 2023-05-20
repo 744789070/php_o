b3dc25ef34eb8e301e2e3fa4beb500a35587926c
BinTang <744789070@qq.com>
2023-05-20 16:50:36 +0800

    -

--- api/chat.php
+++ api/chat.php
@@ -60,9 +60,9 @@ function index()
     // if (isset($_GET['stop'])) {
     //     $gpt_param['stop'] = $_GET['stop'];
     // }
-    if (empty($_POST['messages'])) {
-        return JsonResponse(0, "参数 'messages' 数组");
-    }
+    // if (empty($_POST['messages'])) {
+    //     return JsonResponse(0, "参数 'messages' 数组");
+    // }
 
     $param['messages'] = $_POST['messages'];
     JsonResponse(0, [$_POST,$_POST['messages']]);
